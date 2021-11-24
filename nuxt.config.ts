efe6f7f30eaed0aae1ee97a68121baa063344496
Peter Joseph <p.joseph@oliverhume.com.au>
2021-11-24 14:26:57 +1000

    trying to fix - not working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     build: {
-        transpile: ['@aws-amplify', 'aws-amplify']
-    }
+        transpile: ['@aws-amplify', 'aws-amplify', '@aws-sdk', 'aws-sdk']
+    },
+    alias: {
+        '@aws-amplify/core': '@aws-amplify/core/lib-esm/index.js',
+        '@aws-crypto': '@aws-crypto/sha256-js/build/index.js',
+        '@aws-sdk/util-hex-encoding': '@aws-sdk/util-hex-encoding/dist/cjsf/index.js',
+      }
 })
